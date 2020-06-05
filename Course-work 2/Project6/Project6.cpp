78f23a6a71ecdd0782c5d0705b6363857d0d0c45
Terra13 <40073493+Terra13@users.noreply.github.com>
2020-06-06 00:13:50 +0300

    The final version

---
+++ Course-work 2/Project6/Project6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+//#include "extra.h"
+#include "../../../../Desktop/untitled2/extra.h"
+
+using namespace std;
+
+int main()
+{
+    extra obj;
+    obj.main_cycle();
+
+    return 0;
+}
+
